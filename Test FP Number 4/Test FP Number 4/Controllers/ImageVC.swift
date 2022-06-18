64d0ab0db7f1f8f48a46ddea75fe5c5e4c08a124
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2022-06-18 13:31:23 +0300

    Image Button
    added an array of images

---
+++ Test FP Number 4/Test FP Number 4/Controllers/ImageVC.swift
@@ -0,0 +1,14 @@
+//
+//  ImageVC.swift
+//  Test FP Number 4
+//
+//  Created by Ваня Науменко on 18.06.22.
+//
+
+import UIKit
+
+class ImageVC: UICollectionViewCell {
+    
+    @IBOutlet weak var photoView: UIImageView!
+    
+}
--- Test FP Number 4/Test FP Number 4/Controllers/PhotosCVC.swift
+++ Test FP Number 4/Test FP Number 4/Controllers/PhotosCVC.swift
@@ -9,80 +9,41 @@ import UIKit
 
 private let reuseIdentifier = "Cell"
 
+// MARK: - PhotosCVC
+
 class PhotosCVC: UICollectionViewController {
+    var images = [UIImage]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        for i in 1 ... 19 {
+            let image = UIImage(named: "\(i)")!
+            images.append(image)
+        }
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        let layout = UICollectionViewFlowLayout()
+        let sizeWH = UIScreen.main.bounds.width / 3 - 15
+        layout.itemSize = CGSize(width: sizeWH, height: sizeWH)
+        collectionView.collectionViewLayout = layout
     }
-    */
 
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
-
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        return images.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! ImageVC
+        let image = images[indexPath.item]
+        cell.photoView.image = image
         return cell
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-
 }
